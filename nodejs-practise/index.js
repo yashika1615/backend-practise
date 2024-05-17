d2a31a4eacf1b47fc3300de9f554938a691701c9
Yashika1615 <katariay22@gmail.com>
2024-05-17 22:22:57 +0530

    NODE JS FROM DIPESH MALVIYA CHANNEL

---
+++ nodejs-practise/index.js
@@ -0,0 +1,5 @@
+console.log(" helllo node js");
+console.log("this is the first class on node js");
+console.log("hello world");
+//process.exit(1);
+process.exitCode = 0;
---
+++ script.js
@@ -0,0 +1,21 @@
+// // const name2="dvsj";
+// // console.log(name2);
+
+// const fs= require("fs");
+
+// // // creating a new file
+// // fs.writeFileSync("read.txt","welcome to nodes");
+// // fs.writeFileSync("read.txt","bye bye, welcome to nodes");
+
+// // fs.appendFileSync("read.txt","hello hi bye :>>");
+// /*
+// const buf_data= fs.readFileSync("read.txt");
+// console.log(buf_data);
+
+// org_data= buf_data.toString();
+// console.log(org_data);
+// */
+
+// // to rename a file
+// fs.renameSync("read.txt" ,"readwrite.txt");
+
