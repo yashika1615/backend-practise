207979d5aaf2fb66aaa73abd8c48093f8ab376d3
Yashika1615 <katariay22@gmail.com>
2024-06-14 22:30:38 +0530

    ..

--- file.js
+++ file.js
@@ -5,23 +5,27 @@ const fs = require("fs");
 // synchronous call
 // create a file in current directory whose name will
 // be test.txt and we have to write hey there in that file
-//fs.writeFileSync('./test.txt', "Hello");
+fs.writeFileSync('./test.txt', "Hello");
 
 // after creating file if you chsnge hello wrld to
 // hello it will override
 
 
 // asynchronous
-//fs.writeFile('./test.txt', "Hello world async", (err) => { });
+fs.writeFile('./test.txt', "Hello world async", (err) => { });
 
 // read contacts.txt file
 
 const result = fs.readFile('./contacts.txt', "utf-8", (err, result) => {
-    if (err) {
-        console.log("Error, err");
-    }
-    else {
-        console.log(result);
-    }
-});
-console.log(result);
+     if (err) {
+         console.log("Error, err");
+     }
+     else {
+         console.log(result);
+     }
+ });
+ console.log(result);
+
+//fs.appendFileSync("./test.txt", new Date().getDate().toLocaleString());
+
+fs.appendFileSync("./test.txt", 'Hey there\n');
