0ac8721166dbb56e469cecb3089a96cfd7840451
Yashika1615 <katariay22@gmail.com>
2024-07-20 22:38:05 +0530

    practise

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
     if (req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()}:${req.url} New Req Recieved\n`;
-    console.log(log);
-    fs.appendFile('log.txt', log, (err) => {
+
+    const log = `${Date.now()}: ${req.method} ${req.url} New Req Recieved\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+
+    fs.appendFile("log.txt", log, (err) => {
         if (err) {
             console.error(err);
             res.statusCode = 500;
@@ -13,18 +17,27 @@ const myServer = http.createServer((req, res) => {
         }
 
         if (!res.finished) {
-            switch (req.url) {
-                case '/':
+            switch (myUrl.pathname) {
+                case "/":
                     res.end("home page");
                     break;
-                case '/about':
-                    res.end("hello its me");
+                case "/about":
+                    const username = myUrl.query.myname || "guest";
+                    res.end(`Hi, ${username}`);
+                    break;
+                case "/search":
+                    const search = myUrl.query.search_query || "nothing";
+                    res.end("Here are your results for " + search);
                     break;
                 default:
+                    res.statusCode = 404;
                     res.end("404 not found");
+                    break;
             }
         }
     });
 });
 
-myServer.listen(8000, () => console.log("server started"));
+myServer.listen(8000, () => {
+    console.log("Server Started");
+});
